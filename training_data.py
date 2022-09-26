d1f565915073ce24e53bec30b73932cef14f30ba
bartekkrzepkowski <bartekkrzepkowski@gmail.com>
2022-09-26 17:45:36 +0200

    functionality correction of 3,5,6 + example data

--- training_data.py
+++ training_data.py
@@ -2,19 +2,20 @@
 defaultPrompt = {
     'song_hint': 'Provide the name of the song that {}',
     'facts': 'Tell me {} interesting facts about song {}',
-    'trans': '''
-    Translate the lyrics of the given song into {} language.
+    'trans': '''Translate the lyrics of the given song into {} language.
     
-    The lyrics: {}
+The lyrics:
+Yesterday
+{}
     
-    Translation:
+Translation:
     ''',
-    'interp': '''
-    How do you interpret the words of the indicated song?
-    
-    Song: {}
-    
-    Interpretation:
+    'interp': '''How do you interpret the words of the indicated song?
+
+The Song:
+{}
+
+Interpretation:
     '''
 
 }
